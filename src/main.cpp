afff77cf75640fedc9a95884efbc9895b3ffd3e2
lavochka <name.space.std@outlook.com>
2026-04-07 09:46:28 +0700

    update +multi-client

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,115 @@
 #include <iostream>
 #include <string>
+#include <thread>
 #include <cstring>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+
+void handle_client(int client_fd, sockaddr_in client_addr)
+{
+    char client_ip[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
+
+    std::cout << "Client connected: " << client_ip << ":" << ntohs(client_addr.sin_port) << "\n";
+    std::cout << "-------------------\n";
+
+    std::string welcome_msg = "Welcome, ";
+    welcome_msg += client_ip;
+    welcome_msg += ":";
+    welcome_msg += std::to_string(ntohs(client_addr.sin_port));
+    welcome_msg += "!\n";
+    send(client_fd, welcome_msg.c_str(), welcome_msg.size(), 0);
+
+    while (true)
+    {
+        std::string main_menu = "\n";
+        main_menu += "=== MAIN MENU ===\n";
+        main_menu += "------------------\n";
+        main_menu += "1. Show current ip and port\n";
+        main_menu += "2. Show std tcp return hello\n";
+        main_menu += "3. Return your message\n";
+        main_menu += "4. Show default HTML 'hello'\n";
+        main_menu += "0. Exit\n";
+        main_menu += "Your choice: ";
+
+        send(client_fd, main_menu.c_str(), main_menu.size(), 0);
+
+        char buffer[1024] = {0};
+        int bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
+
+        if (bytes_received > 0)
+        {
+            buffer[bytes_received] = '\0';
+            std::string message = buffer;
+
+            while (!message.empty() && (message.back() == '\n' || message.back() == '\r'))
+            {
+                message.pop_back();
+            }
+
+            std::cout << "Client " << std::to_string(ntohs(client_addr.sin_port)) << "choice: " << message << "\n";
+
+            std::string response;
+
+            if (message == "1")
+            {
+                response = "\nYour IP is: ";
+                response += client_ip;
+                response += " and your port is: ";
+                response += std::to_string(ntohs(client_addr.sin_port));
+                response += "\n";
+                send(client_fd, response.c_str(), response.size(), 0);
+            }
+            else if (message == "2")
+            {
+                response = "\nHello from the server!\n";
+                send(client_fd, response.c_str(), response.size(), 0);
+            }
+            else if (message == "3")
+            {
+                std::string ask = "Enter your message: ";
+                send(client_fd, ask.c_str(), ask.size(), 0);
+
+                char msg_buffer[1024] = {0};
+                int msg_bytes = recv(client_fd, msg_buffer, sizeof(msg_buffer) - 1, 0);
+                if (msg_bytes > 0)
+                {
+                    msg_buffer[msg_bytes] = '\0';
+                    std::string user_msg = msg_buffer;
+                    response = "\nYou said: ";
+                    response += user_msg;
+                    response += "\n";
+                    send(client_fd, response.c_str(), response.size(), 0);
+                }
+            }
+            else if (message == "4")
+            {
+                response = "\n<!DOCTYPE html>\n<html>\n<head>\n<title>My Web Server</title>\n</head>\n<body>\n<h1>Hello, World!</h1>\n<p>This is a simple HTML page served by the C++ server.</p>\n</body>\n</html>\n";
+                send(client_fd, response.c_str(), response.size(), 0);
+            }
+            else if (message == "0")
+            {
+                std::cout << "Client disconnected: " << client_ip << ":" << ntohs(client_addr.sin_port) << "\n";
+                break;
+            }
+            else
+            {
+                response = "\nInvalid choice. Please try again.\n";
+                send(client_fd, response.c_str(), response.size(), 0);
+            }
+        }
+        else
+        {
+            std::cout << "Client disconnected: " << client_ip << ":" << ntohs(client_addr.sin_port) << "\n";
+            break;
+        }
+    }
+
+    close(client_fd);
+}
+
 
 int main()
 {
@@ -51,126 +156,8 @@ int main()
             continue;
         }
 
-        char client_ip[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
-
-        std::cout << "Client connected: " << client_ip << ":" << ntohs(client_addr.sin_port) << "\n";
-        std::cout << "-------------------\n";
-
-        std::string welcome_msg = "Welcome, ";
-            welcome_msg += client_ip;
-            welcome_msg += ":";
-            welcome_msg += std::to_string(ntohs(client_addr.sin_port));
-            welcome_msg += "!\n";
-        send(client_fd, welcome_msg.c_str(), welcome_msg.size(), 0);
-
-        while (true)
-        {
-            std::string main_menu = "\n";
-            main_menu += "=== MAIN MENU ===\n";
-            main_menu += "------------------\n";
-            main_menu += "1. Show current ip and port\n";
-            main_menu += "2. Show std tcp return hello\n";
-            main_menu += "3. Return your message\n";
-            main_menu += "4. Show default HTML 'hello'\n";
-            main_menu += "0. Exit\n";
-            main_menu += "Your choice: ";
-
-            send(client_fd, main_menu.c_str(), main_menu.size(), 0);
-
-            char buffer[1024] = {0};
-            int bytes_received = recv(client_fd, buffer, sizeof(buffer) - 1, 0);
-
-            if (bytes_received > 0)
-            {
-                buffer[bytes_received] = '\0';
-                std::string message = buffer;
-
-                while (!message.empty() && (message.back() == '\n' || message.back() == '\r'))
-                {
-                    message.pop_back();
-                }
-
-                std::cout << "Client choice: " << message << "\n";
-
-                std::string response;
-
-                if (message == "1")
-                {
-                    response = "\nYour IP is: ";
-                    response += client_ip;
-                    response += " and your port is: ";
-                    response += std::to_string(ntohs(client_addr.sin_port));
-                    response += "\n";
-                    send(client_fd, response.c_str(), response.size(), 0);
-                }
-                else if (message == "2")
-                {
-                    response = "\nHello from the server!\n";
-                    send(client_fd, response.c_str(), response.size(), 0);
-                }
-                else if (message == "3")
-                {
-                    std::string ask = "Enter your message: ";
-                    send(client_fd, ask.c_str(), ask.size(), 0);
-
-                    char message_buffer[1024] = {0};
-                    int message_bytes = recv(client_fd, message_buffer, sizeof(message_buffer) - 1, 0);
-
-                    if (message_bytes > 0)
-                    {
-                        message_buffer[message_bytes] = '\0';
-                        std::string user_message = message_buffer;
-
-                        while (!user_message.empty() && (user_message.back() == '\n' || user_message.back() == '\r'))
-                        {
-                            user_message.pop_back();
-                        }
-
-                        std::string response = "\nYou said:";
-                        response += user_message;
-                        response += "\n";
-
-                        send(client_fd, response.c_str(), response.size(), 0);
-                    }
-                }
-                else if (message == "4")
-                {
-                    response =
-                        "\n<html>\n"
-                        "   <body>\n"
-                        "      <h1>Hello from the server!</h1>\n"
-                        "   </body>\n"
-                        "</html>\n";
-                    send(client_fd, response.c_str(), response.size(), 0);
-                }
-                else if (message == "0")
-                {
-                    response = "\nGoodbye, ";
-                    response += client_ip;
-                    response += "!\n";
-                    send(client_fd, response.c_str(), response.size(), 0);
-                    break;
-                }
-                else
-                {
-                    response = "\nInvalid choice. Please try again.\n";
-                    send(client_fd, response.c_str(), response.size(), 0);
-                }
-            }
-            else if (bytes_received == 0)
-            {
-                std::cout << "Client disconnected\n";
-                break;
-            }
-            else
-            {
-                std::cerr << "recv error\n";
-                break;
-            }
-        }
-
-        close(client_fd);
+        std::thread client_thread(handle_client, client_fd, client_addr);
+        client_thread.detach();
     }
 
     close(server_fd);
